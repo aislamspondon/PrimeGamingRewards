bc77f452e1faa19125c3e3d36c70e3c4df4e0c58
Sushen Biswas <sushenbiswasaga@gmail.com>
2022-05-24 17:25:19 +0600

    Add : POM (Page Object Model) Frame Work

--- PrimeGaming.py
+++ PrimeGaming.py
@@ -5,8 +5,30 @@ from selenium import webdriver
 driver = webdriver.Chrome("Driver/chromedriver.exe")
 
 
-driver.get("https://mail.google.com/")
+driver.get("https://accounts.google.com/?hl=en-us")
 time.sleep(4)
+
+
+def login_gmail(username, password):
+    button_xpath = "//tp-yt-paper-button[@aria-label='Sign in']"
+    login_button = driver.find_elements_by_xpath(button_xpath)
+    login_button[0].click()
+    print(len(login_button))
+    time.sleep(4)
+
+    driver.implicitly_wait(10)
+    # driver.get('https://accounts.google.com/')
+    password_xpath = "//input[@type='email']"
+    driver.find_element_by_xpath(password_xpath).send_keys(username)
+
+    next_button_xpath = "//span[contains(.,'Next')]/parent::button"
+    next_button = driver.find_element_by_xpath(next_button_xpath)
+    next_button.click()
+    print(next_button.text)
+    time.sleep(4)
+
+
+login_gmail("something@gmail.com", "12121212")
 
 
 print(input("Google Login Done ....\n"))
