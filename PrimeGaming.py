a2037415d381f81fbec111a68f74523d7af5acda
user <sushenbiswasaga@021>
2022-05-24 15:24:35 +0600

    add : .gitignore, requirements.txt

--- PrimeGaming.py
+++ PrimeGaming.py
@@ -3,5 +3,16 @@ import time
 
 from selenium import webdriver
 driver = webdriver.Chrome("chromedriver.exe")
-driver.get("https://gaming.amazon.com/intro")
 
+
+driver.get("https://mail.google.com/")
+time.sleep(4)
+
+driver.get("https://www.twitch.tv/")
+time.sleep(4)
+
+driver.get("https://www.amazon.com/your-account")
+time.sleep(4)
+
+driver.get("https://gaming.amazon.com/intro")
+time.sleep(4)
