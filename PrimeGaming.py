04a2e278fcf006a85d37e2fed68c729c071cd444
user <sushenbiswasaga@021>
2022-05-24 12:22:15 +0600

    Collect Prime Game Web Address

---
+++ PrimeGaming.py
@@ -0,0 +1,7 @@
+""" This script can add subscriber to prime games"""
+import time
+
+from selenium import webdriver
+driver = webdriver.Chrome("chromedriver.exe")
+driver.get("https://gaming.amazon.com/intro")
+
