0e5be8c165b0732719c4d2bac915568c31062e18
Sushen Biswas <sushenbiswasaga@gmail.com>
2022-05-25 07:28:55 +0600

    Update: PrimeGaming.py "Enable Login"

--- PrimeGaming.py
+++ PrimeGaming.py
@@ -1,34 +1,77 @@
 """ This script can add subscriber to prime games"""
+import random
 import time
-
+import pathlib
+import gspread
+from google.oauth2.service_account import Credentials
 from selenium import webdriver
-driver = webdriver.Chrome("Driver/chromedriver.exe")
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium_stealth import stealth
+
+chrome_options = Options()
+# scriptDirectory = pathlib.Path().absolute()
+chrome_options.add_argument("--start-maximized")
 
 
+#
+# chrome_options.add_argument("--user-data-dir=chrome-data")
+# chrome_options.add_argument('--profile-directory=Profile 8')
+# prefs = {"profile.default_content_setting_values.notifications": 2}
+# chrome_options.add_experimental_option("prefs", prefs)
+# chrome_options.add_argument('disable-infobars')
+# chrome_options.add_argument("user-data-dir=chrome-data")
+# chrome_options.add_argument(f"user-data-dir={scriptDirectory}\\userdata")
+
+chrome_options.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
+chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+chrome_options.add_experimental_option("useAutomationExtension", False)
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_argument('--disable-web-security')
+chrome_options.add_argument('--allow-running-insecure-content')
+
+driver = webdriver.Chrome("Driver/chromedriver.exe", chrome_options=chrome_options)
+
+stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+        )
+
+# Time Counting
+StartTime = time.time()
+print("This Script Start " + time.ctime())
+
+
+# driver.get("https://accounts.google.com/")
 driver.get("https://accounts.google.com/?hl=en-us")
 time.sleep(4)
 
 
-def login_gmail(username, password):
-    button_xpath = "//tp-yt-paper-button[@aria-label='Sign in']"
-    login_button = driver.find_elements_by_xpath(button_xpath)
-    login_button[0].click()
-    print(len(login_button))
-    time.sleep(4)
 
+def enter_email_password(driver, username, password):
     driver.implicitly_wait(10)
-    # driver.get('https://accounts.google.com/')
-    password_xpath = "//input[@type='email']"
-    driver.find_element_by_xpath(password_xpath).send_keys(username)
+    email_xpath = "//input[@type='email']"
+    driver.find_element_by_xpath(email_xpath).send_keys(username)
+    print(input("Try Manually :"))
 
     next_button_xpath = "//span[contains(.,'Next')]/parent::button"
     next_button = driver.find_element_by_xpath(next_button_xpath)
     next_button.click()
-    print(next_button.text)
     time.sleep(4)
 
+    driver.implicitly_wait(10)
+    password_xpath = "//input[@type='password']"
+    driver.find_element_by_xpath(password_xpath).send_keys(password)
+    driver.find_element_by_xpath(next_button_xpath).click()
 
-login_gmail("something@gmail.com", "12121212")
+
+enter_email_password(driver, "johnalis841@gmail.com", "globe0112358")
 
 
 print(input("Google Login Done ....\n"))
---
+++ experiment.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium_stealth import stealth
+
+options = webdriver.ChromeOptions()
+options.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+options.add_argument('--disable-blink-features=AutomationControlled')
+driver = webdriver.Chrome("Driver/chromedriver.exe", options=options)
+stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+        )
+driver.get("https://www.google.com")
