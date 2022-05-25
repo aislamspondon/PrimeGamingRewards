32ed3ab53b5d0c4490087a649cb387acf05ce34a
Asraful Islam Spondon <asrafulislamais@gmail.com>
2022-05-25 11:20:53 +0600

    UPDATE: NoTouch.py "script"

--- NoTouch.py
+++ NoTouch.py
@@ -13,38 +13,9 @@ screen_size = pyautogui.size()
 print(screen_size)
 
 speed_frequency = 4
-
+gmail_link = "aislamspondon@gmail.com"
+# pyautogui.typewrite(f'{gmail_link}\n', interval=.001)
 # print(input("press enter to start:"))
-
-open_browser_move_x = 710
-open_browser_move_y = 1058
-open_browser_move = pyautogui.moveTo(open_browser_move_x, open_browser_move_y)
-time.sleep(speed_frequency)
-pyautogui.click()
-
-# print(input("Select Profile:"))
-
-open_profile_move_x = 691
-open_profile_move_y = 606
-open_profile_move = pyautogui.moveTo(open_profile_move_x, open_profile_move_y)
-time.sleep(speed_frequency)
-pyautogui.click()
-
-# print(input("Maximize:"))
-pyautogui.hotkey('Alt', 'Space', 'x')
-
-# print(input("Enter Gmail Url:"))
-
-open_gmail_move_x = 146
-open_gmail_move_y = 51
-gmail_url_move = pyautogui.moveTo(open_gmail_move_x, open_gmail_move_y)
-time.sleep(speed_frequency)
-pyautogui.click()
-
-gmail_link = "https://mail.google.com/mail/"
-pyautogui.typewrite(f'{gmail_link}\n', interval=.001)
-
-print(input("Enter TwisTv Url:"))
 # print('Press Ctrl-C to quit.')
 # try:
 #     while True:
@@ -55,14 +26,37 @@ print(input("Enter TwisTv Url:"))
 # except KeyboardInterrupt:
 #     print('\n')
 
-open_twistTv_move_x = 146
-open_twistTv_move_y = 51
-twistTv_url_move = pyautogui.moveTo(open_twistTv_move_x, open_twistTv_move_y)
+open_browser_move_x = 1873
+open_browser_move_y = 739
+open_browser_move = pyautogui.moveTo(open_browser_move_x, open_browser_move_y)
+time.sleep(speed_frequency)
+pyautogui.click()
+time.sleep(speed_frequency)
+pyautogui.hotkey('ctrl', 'Shift', 'M')
+
+time.sleep(speed_frequency)
+for i in range(9):
+    pyautogui.hotkey('tab')
+    time.sleep(0.25)
+
+pyautogui.hotkey('Enter')
+time.sleep(1)
+pyautogui.hotkey('tab')
+pyautogui.hotkey('Enter')
+time.sleep(3)
+
+
+print(input("Select Profile:"))
+
+open_profile_move_x = 691
+open_profile_move_y = 606
+open_profile_move = pyautogui.moveTo(open_profile_move_x, open_profile_move_y)
 time.sleep(speed_frequency)
 pyautogui.click()
 
-twitch_link = "https://www.twitch.tv/twistv"
-pyautogui.typewrite(f'{twitch_link}\n', interval=.001)
+
+
+# print(input("Maximize:"))
 
 
 
